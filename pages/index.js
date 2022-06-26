c44de0ca24217065d692527145528cc14dc7d884
fiatjaf <fiatjaf@gmail.com>
2022-06-26 20:02:55 -0300

    add link to gateway github repository.

--- pages/index.js
+++ pages/index.js
@@ -58,6 +58,17 @@ export default function Home() {
         <a href="https://t.me/nostr_protocol">our Telegram group</a> while we
         don&apos;t have a decent group chat application fully working on Nostr.
       </p>
+
+      <br />
+
+      <h2>Contribute to this site!</h2>
+      <p>
+        You can find the source code for this gateway website at{' '}
+        <a href="https://github.com/fiatjaf/nostr-gateway">
+          https://github.com/fiatjaf/nostr-gateway
+        </a>{' '}
+        while we haven&apos;t finished implementing Git over Nostr yet.
+      </p>
     </div>
   )
 }
