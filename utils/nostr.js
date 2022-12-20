7eb3200398fae00f5f1a5e0596fad1349986a5d0
obelisk-dev <bheishman@protonmail.com>
2022-12-20 14:05:59 -0500

    make "kindNames" an object/add channel "kinds"

--- utils/nostr.js
+++ utils/nostr.js
@@ -1,13 +1,19 @@
 import {bech32} from 'bech32'
 
-export const kindNames = [
-  /* 0: */ 'profile metadata',
-  /* 1: */ 'text note',
-  /* 2: */ 'relay recommendation',
-  /* 3: */ 'contact list',
-  /* 4: */ 'encrypted direct message',
-  /* 5: */ 'event deletion'
-]
+export const kindNames = {
+  0: 'profile metadata',
+  1: 'text note',
+  2: 'relay recommendation',
+  3: 'contact list',
+  4: 'encrypted direct message',
+  5: 'event deletion',
+  7: 'reaction',
+  40: 'channel creation',
+  41: 'channel metadata',
+  42: 'channel message',
+  43: 'channel hide message',
+  44: 'channel mute user'
+}
 
 export const relays = [
   'wss://nostr-pub.wellorder.net',
