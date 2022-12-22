304db463dc9761f983a006b549876aade4046653
Lee Salminen <leesalminen@gmail.com>
2022-12-22 13:29:20 -0600

    allow users to republish to custom relays

--- components/event.js
+++ components/event.js
@@ -16,12 +16,19 @@ export default function Event({id, event}) {
       const pool = relayPool()
       relays.forEach(r => pool.addRelay(r, {read: true, write: true}))
 
+      let additionalRelays = prompt("Is there an additional relay you want to broadcast to? You may enter a comma separated list here.")
+
+      if(additionalRelays.indexOf("wss://") === 0) {
+        additionalRelays = additionalRelays.split(',')
+        additionalRelays.forEach(r => pool.addRelay(r.trim(), {read: true, write: true}))
+      }
+
       const ev = await pool.publish(event, (status, url) => {
         if (status === 0) {
           console.log(`publish request sent to ${url}`)
         }
         if (status === 1) {
-          console.log(`event published by ${url}`, ev)
+          console.log(`event published by ${url}`)
         }
       })
     })
