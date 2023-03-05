d6e07485685e8ea15a556682323686972eb963b0
fiatjaf <fiatjaf@gmail.com>
2023-03-05 15:17:48 -0300

    update kind names and list of relays.

--- utils/nostr.js
+++ utils/nostr.js
@@ -7,27 +7,41 @@ export const kindNames = {
   3: 'contact list',
   4: 'encrypted direct message',
   5: 'event deletion',
+  6: 'repost',
   7: 'reaction',
+  8: 'badge award',
   40: 'channel creation',
   41: 'channel metadata',
   42: 'channel message',
   43: 'channel hide message',
-  44: 'channel mute user'
+  44: 'channel mute user',
+  1984: 'report',
+  9735: 'zap',
+  9734: 'zap request',
+  10002: 'relay list',
+  30008: 'profile badges',
+  30009: 'badge definition',
+  30078: 'app-specific data',
+  30023: 'article'
 }
 
 export const fallbackRelays = [
-  'wss://nostr-relay.untethr.me',
   'wss://nostr-pub.wellorder.net',
-  'wss://nostr.zebedee.social',
+  'wss://nostr.zebedee.cloud',
   'wss://offchain.pub',
   'wss://nostr.fmt.wiz.biz',
-  'wss://nostr-relay.wlvs.space',
+  'wss://eden.nostr.land',
+  'wss://atlas.nostr.land',
   'wss://relay.snort.social',
   'wss://nostr.fly.dev',
   'wss://nostr.nostr.band',
-  'wss://relay.realsearch.cc',
   'wss://relay.nostrgraph.net',
-  'wss://relay.minds.com/nostr/v1/ws'
+  'wss://relay.nostr.bg',
+  'wss://nostr.wine',
+  'wss://nos.lol',
+  'wss://relay.mostr.pub',
+  'wss://no.str.cr',
+  'wss://brb.io'
 ]
 
 export function nip05toURL(identifier) {
