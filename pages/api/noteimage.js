41045441b1d7a698f634f656e0759caa931b92ad
fiatjaf <fiatjaf@gmail.com>
2023-03-20 10:05:43 -0300

    without monospace the `charsPerLine` strategy does not work anymore, use `ctx.measureText()` instead.

--- pages/api/noteimage.js
+++ pages/api/noteimage.js
@@ -16,10 +16,9 @@ function textToImage(text, previewStyle) {
   const y = 10
   const lineHeight = 17
   const width = 410
-  const charsPerLine = 52
   const maxHeight = previewStyle === 'twitter' ? width / 1.91 : width * 1.5
   const height = Math.min(
-    (text.length / (charsPerLine - 12) + 2) * 17,
+    (text.length / 38 + 2) * 17, // fine-tuned parameters
     maxHeight
   )
 
@@ -30,8 +29,7 @@ function textToImage(text, previewStyle) {
   ctx.font = '16px serif'
 
   let currentLineNumber = 1
-  let currentLineText = ''
-  let currentLineChars = 0
+  let currentLine = ''
 
   for (let i = 0; i < text.length; i++) {
     let char = text[i]
@@ -41,7 +39,7 @@ function textToImage(text, previewStyle) {
       continue
     }
     if (char === ' ') {
-      if (currentLineChars === charsPerLine) {
+      if (ctx.measureText(currentLine).width > width - x * 3) {
         printLine()
         continue
       }
@@ -49,23 +47,25 @@ function textToImage(text, previewStyle) {
       if (untilNextSpace === -1) {
         untilNextSpace = text.length - (i + 1) // until end
       }
-      if (currentLineChars + untilNextSpace > charsPerLine) {
+      if (
+        ctx.measureText(currentLine + text.slice(i + 1, i + 1 + untilNextSpace))
+          .width >
+        width - x * 3
+      ) {
         printLine()
         continue
       }
     }
 
-    currentLineText += char
-    currentLineChars++
+    currentLine += char
   }
 
-  if (currentLineChars) printLine()
+  if (currentLine.length) printLine()
 
   function printLine() {
-    ctx.fillText(currentLineText, x, y + lineHeight * currentLineNumber)
+    ctx.fillText(currentLine, x, y + lineHeight * currentLineNumber)
     currentLineNumber++
-    currentLineText = ''
-    currentLineChars = 0
+    currentLine = ''
   }
 
   // background
