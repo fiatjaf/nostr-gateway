d3f80434895337b6f347271f084e826ca158c240
fiatjaf <fiatjaf@gmail.com>
2023-03-20 12:23:46 -0300

    change to sans-serif and recalculate everything.
    for some reason only sans-serif works on vercel.

--- pages/api/noteimage.js
+++ pages/api/noteimage.js
@@ -17,55 +17,15 @@ function textToImage(text, previewStyle) {
   const lineHeight = 17
   const width = 410
   const maxHeight = previewStyle === 'twitter' ? width / 1.91 : width * 1.5
-  const height = Math.min(
-    (text.length / 38 + 2) * 17, // fine-tuned parameters
-    maxHeight
-  )
-
+  const lines = computeLines(text, width - x * 2)
+  const height = Math.min(lines.length * 17 + x * 3, maxHeight)
   const canvas = createCanvas(width, height)
   const ctx = canvas.getContext('2d')
 
   // text
-  ctx.font = '16px serif'
-
-  let currentLineNumber = 1
-  let currentLine = ''
-
-  for (let i = 0; i < text.length; i++) {
-    let char = text[i]
-
-    if (char === '\n') {
-      printLine()
-      continue
-    }
-    if (char === ' ') {
-      if (ctx.measureText(currentLine).width > width - x * 3) {
-        printLine()
-        continue
-      }
-      let untilNextSpace = text.slice(i + 1).indexOf(' ')
-      if (untilNextSpace === -1) {
-        untilNextSpace = text.length - (i + 1) // until end
-      }
-      if (
-        ctx.measureText(currentLine + text.slice(i + 1, i + 1 + untilNextSpace))
-          .width >
-        width - x * 3
-      ) {
-        printLine()
-        continue
-      }
-    }
-
-    currentLine += char
-  }
-
-  if (currentLine.length) printLine()
-
-  function printLine() {
-    ctx.fillText(currentLine, x, y + lineHeight * currentLineNumber)
-    currentLineNumber++
-    currentLine = ''
+  ctx.font = '16px sans-serif'
+  for (let i = 0; i < lines.length; i++) {
+    ctx.fillText(lines[i], x, y + lineHeight * (i + 1))
   }
 
   // background
@@ -74,4 +34,49 @@ function textToImage(text, previewStyle) {
   ctx.fillRect(0, 0, width, height)
 
   return canvas
+}
+
+function computeLines(text, maxLineWidth) {
+  // throwaway canvas object just for measuring text
+  const canvas = createCanvas(1000, 1000)
+  const ctx = canvas.getContext('2d')
+  ctx.font = '16px sans-serif'
+
+  let lines = []
+  let currentLine = ''
+
+  for (let i = 0; i < text.length; i++) {
+    let char = text[i]
+
+    if (char === '\n') {
+      lines.push(currentLine)
+      currentLine = ''
+      continue
+    }
+    if (char === ' ') {
+      if (ctx.measureText(currentLine).width > maxLineWidth) {
+        lines.push(currentLine)
+        currentLine = ''
+        continue
+      }
+      let untilNextSpace = text.slice(i + 1).indexOf(' ')
+      if (untilNextSpace === -1) {
+        untilNextSpace = text.length - (i + 1) // until end
+      }
+
+      let textUntilNextSpace =
+        currentLine + text.slice(i + 1, i + 1 + untilNextSpace)
+      if (ctx.measureText(textUntilNextSpace).width > maxLineWidth) {
+        lines.push(currentLine)
+        currentLine = ''
+        continue
+      }
+    }
+
+    currentLine += char
+  }
+
+  if (currentLine.length) lines.push(currentLine)
+
+  return lines
 }
